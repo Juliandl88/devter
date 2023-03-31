5d13a0969f13a2040fdb8157d08c0d5837d9c38f
Julian <julian_delisio@hotmail.com>
2023-03-31 16:53:57 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1>
+          App <a href="https://nextjs.org">Devter</a>
         </h1>
-        
+        <style jsx>{`
+          h1 {
+            text-align: center;
+            font-size: 48px;
+          }
+          a{
+            color: violet;
+            padding: 20px
+          }
+        `}
+        </style>
    
 
     </div>
